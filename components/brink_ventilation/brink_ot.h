68b46b0d9adffc1a7249f94b037ba9832084e459
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-01 17:24:47 +0100

    Refactor BrinkOpenTherm and add BrinkVentilationNumber
    Removed unnecessary comments and logging in the setup and set_ventilation_level methods. Added a new BrinkVentilationNumber class for controlling ventilation levels.

--- components/brink_ventilation/brink_ot.h
+++ components/brink_ventilation/brink_ot.h
@@ -10,7 +10,6 @@ class BrinkOpenTherm : public PollingComponent {
   OpenTherm ot;
   int in_pin, out_pin;
 
-  // Sensory i Sterowanie
   sensor::Sensor *current_vent_sensor{nullptr};
   sensor::Sensor *supply_temp_sensor{nullptr};
   sensor::Sensor *exhaust_temp_sensor{nullptr};
@@ -19,16 +18,14 @@ class BrinkOpenTherm : public PollingComponent {
 
   void setup() override {
     ot.begin(in_pin, out_pin);
-    ESP_LOGCONFIG("brink", "OpenTherm setup na pinach IN:%d, OUT:%d", in_pin, out_pin);
   }
 
-  // Settery dla Pythona
   void set_current_vent_sensor(sensor::Sensor *s) { current_vent_sensor = s; }
   void set_supply_temp_sensor(sensor::Sensor *s) { supply_temp_sensor = s; }
   void set_exhaust_temp_sensor(sensor::Sensor *s) { exhaust_temp_sensor = s; }
 
   void update() override {
-    // ID 77: Aktualna moc wentylacji (Relative ventilation)
+    // ID 77: Aktualna moc wentylacji
     unsigned long response = ot.sendRequest(ot.buildRequest(OpenThermMessageType::Read_Data, 77, 0));
     if (ot.isValidResponse(response) && current_vent_sensor != nullptr) {
         current_vent_sensor->publish_state(ot.getFloat(response));
@@ -48,15 +45,17 @@ class BrinkOpenTherm : public PollingComponent {
   }
 
   void set_ventilation_level(float level) {
-    // ID 71: Sterowanie (Set Ventilation Level)
-    // Konwersja na format f8.8 (wartość * 256)
     uint16_t data = ot.temperatureToData(level); 
-    unsigned long response = ot.sendRequest(ot.buildRequest(OpenThermMessageType::Write_Data, 71, data));
-    
-    if (ot.isValidResponse(response)) {
-        ESP_LOGD("brink", "Ustawiono wentylację na %.1f%%", level);
-    } else {
-        ESP_LOGE("brink", "Błąd zapisu wentylacji!");
-    }
+    ot.sendRequest(ot.buildRequest(OpenThermMessageType::Write_Data, 71, data));
+  }
+};
+
+class BrinkVentilationNumber : public number::Number {
+ public:
+  BrinkOpenTherm *parent_;
+  void set_parent(BrinkOpenTherm *parent) { parent_ = parent; }
+  void control(float value) override {
+    this->publish_state(value);
+    parent_->set_ventilation_level(value);
   }
 };
