ec360c199d4b8a86f0fb651be759092fcc008e5d
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-01 21:22:50 +0100

    Update number.py

--- components/brink_ventilation/number.py
+++ components/brink_ventilation/number.py
@@ -1,21 +1,24 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import number
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_NAME, CONF_ICON
 from . import BRINK_VENTILATION_ID, BrinkOpenTherm
 
-# Definiujemy klasę BrinkNumber w Pythonie, aby pasowała do C++
+# Definiujemy klasę BrinkNumber w namespace
 brink_ventilation_ns = cg.esphome_ns.namespace("brink_ventilation")
 BrinkNumber = brink_ventilation_ns.class_("BrinkNumber", number.Number)
 
-CONFIG_SCHEMA = number.NUMBER_SCHEMA.extend({
+CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(BrinkNumber),
     cv.GenerateID(BRINK_VENTILATION_ID): cv.use_id(BrinkOpenTherm),
+    cv.Optional(CONF_NAME, default="Brink Nastawa Mocy"): cv.string,
+    cv.Optional(CONF_ICON, default="mdi:fan"): cv.icon,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     parent = await cg.get_variable(config[BRINK_VENTILATION_ID])
     var = cg.new_variable(config[CONF_ID])
+    # Rejestrujemy numer z domyślnymi wartościami dla Brinka (0-100%)
     await number.register_number(var, config, min_value=0, max_value=100, step=1)
     cg.add(var.set_parent(parent))
     cg.add(parent.set_ventilation_number(var))
