bb6709e735ce5e9a24e0e570c24b03a65287a52b
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-01 21:11:39 +0100

    Create text_sensor.py

---
+++ components/brink_ventilation/text_sensor.py
@@ -0,0 +1,21 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import text_sensor
+from esphome.const import CONF_ID, CONF_TYPE
+from . import BRINK_VENTILATION_ID, BrinkVentilation
+
+TYPES = {
+    "STATUS": text_sensor.TEXT_SENSOR_SCHEMA.extend(cv.COMPONENT_SCHEMA),
+}
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(BRINK_VENTILATION_ID): cv.use_id(BrinkVentilation),
+        cv.Required(CONF_TYPE): cv.one_of(*TYPES, upper=True),
+    }
+).extend(text_sensor.TEXT_SENSOR_SCHEMA)
+
+async def to_code(config):
+    parent = await cg.get_variable(config[BRINK_VENTILATION_ID])
+    var = await text_sensor.new_text_sensor(config)
+    cg.add(parent.set_status_text_sensor(var))
