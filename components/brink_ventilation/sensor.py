7ca48e2d4a280039c5d2101c8c4c61377f90020b
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-01 21:13:29 +0100

    Update sensor.py

--- components/brink_ventilation/sensor.py
+++ components/brink_ventilation/sensor.py
@@ -2,58 +2,26 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import (
-    CONF_ID,
-    CONF_TYPE,
-    DEVICE_CLASS_TEMPERATURE,
-    STATE_CLASS_MEASUREMENT,
-    UNIT_CELSIUS,
-    UNIT_PERCENT,
+    CONF_ID, CONF_TYPE, DEVICE_CLASS_TEMPERATURE, 
+    STATE_CLASS_MEASUREMENT, UNIT_CELSIUS
 )
-from . import BRINK_VENTILATION_ID, BrinkVentilation
+from . import BRINK_VENTILATION_ID, BrinkOpenTherm
 
-# Definicja typów sensorów
 TYPES = {
-    "T_SUPPLY_IN": sensor.sensor_schema(
-        unit_of_measurement=UNIT_CELSIUS,
-        accuracy_decimals=1,
-        device_class=DEVICE_CLASS_TEMPERATURE,
-        state_class=STATE_CLASS_MEASUREMENT,
-    ),
-    "T_SUPPLY_OUT": sensor.sensor_schema(
-        unit_of_measurement=UNIT_CELSIUS,
-        accuracy_decimals=1,
-        device_class=DEVICE_CLASS_TEMPERATURE,
-        state_class=STATE_CLASS_MEASUREMENT,
-    ),
-    "T_EXHAUST_IN": sensor.sensor_schema(
-        unit_of_measurement=UNIT_CELSIUS,
-        accuracy_decimals=1,
-        device_class=DEVICE_CLASS_TEMPERATURE,
-        state_class=STATE_CLASS_MEASUREMENT,
-    ),
-    "T_EXHAUST_OUT": sensor.sensor_schema(
-        unit_of_measurement=UNIT_CELSIUS,
-        accuracy_decimals=1,
-        device_class=DEVICE_CLASS_TEMPERATURE,
-        state_class=STATE_CLASS_MEASUREMENT,
-    ),
-    "RELATIVE_VENTILATION": sensor.sensor_schema(
-        unit_of_measurement="m³/h", # Zmieniamy na m3/h bo kod składa bajty
-        accuracy_decimals=0,
-        state_class=STATE_CLASS_MEASUREMENT,
-    ),
+    "T_SUPPLY_IN": sensor.sensor_schema(unit_of_measurement=UNIT_CELSIUS, accuracy_decimals=1, device_class=DEVICE_CLASS_TEMPERATURE, state_class=STATE_CLASS_MEASUREMENT),
+    "T_SUPPLY_OUT": sensor.sensor_schema(unit_of_measurement=UNIT_CELSIUS, accuracy_decimals=1, device_class=DEVICE_CLASS_TEMPERATURE, state_class=STATE_CLASS_MEASUREMENT),
+    "T_EXHAUST_IN": sensor.sensor_schema(unit_of_measurement=UNIT_CELSIUS, accuracy_decimals=1, device_class=DEVICE_CLASS_TEMPERATURE, state_class=STATE_CLASS_MEASUREMENT),
+    "T_EXHAUST_OUT": sensor.sensor_schema(unit_of_measurement=UNIT_CELSIUS, accuracy_decimals=1, device_class=DEVICE_CLASS_TEMPERATURE, state_class=STATE_CLASS_MEASUREMENT),
+    "CURRENT_FLOW": sensor.sensor_schema(unit_of_measurement="m³/h", accuracy_decimals=0, state_class=STATE_CLASS_MEASUREMENT),
+    "PRESSURE_IN": sensor.sensor_schema(unit_of_measurement="Pa", accuracy_decimals=0, state_class=STATE_CLASS_MEASUREMENT),
 }
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(BRINK_VENTILATION_ID): cv.use_id(BrinkVentilation),
-        cv.Required(CONF_TYPE): cv.one_of(*TYPES, upper=True),
-    }
-).extend(sensor.SENSOR_SCHEMA).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(BRINK_VENTILATION_ID): cv.use_id(BrinkOpenTherm),
+    cv.Required(CONF_TYPE): cv.one_of(*TYPES, upper=True),
+}).extend(sensor.SENSOR_SCHEMA)
 
 async def to_code(config):
     parent = await cg.get_variable(config[BRINK_VENTILATION_ID])
     var = await sensor.new_sensor(config)
-    
-    # Mapowanie typu z YAML na funkcję w C++
     cg.add(getattr(parent, f"set_{config[CONF_TYPE].lower()}_sensor")(var))
