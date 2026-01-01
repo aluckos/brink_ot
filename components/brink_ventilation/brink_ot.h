1f3e47fd0bf0091cfebcf06afc9556dc8d669fa0
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-01 18:17:44 +0100

    Refactor BrinkOpenTherm class for better pin handling
    Refactor BrinkOpenTherm class to use instance variables for pins and improve interrupt handling.

--- components/brink_ventilation/brink_ot.h
+++ components/brink_ventilation/brink_ot.h
@@ -6,21 +6,41 @@
 namespace esphome {
 namespace brink_ventilation {
 
+// Musimy użyć zmiennych globalnych, aby handler przerwań widział obiekt OT
+static int static_in_pin;
+static int static_out_pin;
+
+// Pusta funkcja obsługi przerwań
 static void IRAM_ATTR handleInterrupt() { }
 
 class BrinkOpenTherm : public PollingComponent {
  public:
   OpenTherm ot;
+  int pin_in;
+  int pin_out;
+
   sensor::Sensor *current_vent_sensor{nullptr};
   sensor::Sensor *supply_temp_sensor{nullptr};
   sensor::Sensor *exhaust_temp_sensor{nullptr};
   
-  BrinkOpenTherm(int in_pin, int out_pin) 
-      : PollingComponent(10000), ot(in_pin, out_pin) {}
+  BrinkOpenTherm(int in, int out) : PollingComponent(10000), pin_in(in), pin_out(out) {
+      static_in_pin = in;
+      static_out_pin = out;
+  }
 
   void setup() override {
+    // Ręczne ustawienie pinów - to często rozwiązuje problem na ESP8266
+    pinMode(pin_in, INPUT);
+    pinMode(pin_out, OUTPUT);
+    
+    // Inicjalizacja z handlerem (identycznie jak w brink_openhab)
     ot.begin(handleInterrupt);
-    ESP_LOGI("brink", "Inicjalizacja OpenTherm zakończona.");
+    
+    // Nadpisujemy piny bezpośrednio w obiekcie, jeśli konstruktor nie zadziałał
+    ot.in_pin = pin_in;
+    ot.out_pin = pin_out;
+
+    ESP_LOGI("brink", "Inicjalizacja pinu IN:%d OUT:%d", pin_in, pin_out);
   }
 
   void set_current_vent_sensor(sensor::Sensor *s) { current_vent_sensor = s; }
@@ -28,40 +48,31 @@ class BrinkOpenTherm : public PollingComponent {
   void set_exhaust_temp_sensor(sensor::Sensor *s) { exhaust_temp_sensor = s; }
 
   void update() override {
-    ESP_LOGD("brink", "Wysyłam zapytanie o status (ID 77)...");
+    // Sprawdzamy status komunikacji
+    unsigned long response = ot.sendRequest(ot.buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)77, 0));
     
-    unsigned long request77 = ot.buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)77, 0);
-    unsigned long response77 = ot.sendRequest(request77);
-    
-    if (ot.isValidResponse(response77)) {
-        float val = ot.getUInt(response77);
-        ESP_LOGI("brink", "Otrzymano odpowiedź ID 77: %.1f", val);
+    if (ot.isValidResponse(response)) {
+        float val = ot.getUInt(response);
         if (current_vent_sensor != nullptr) current_vent_sensor->publish_state(val);
+        ESP_LOGD("brink", "Odczytano moc: %.1f", val);
     } else {
-        ESP_LOGE("brink", "Błąd odpowiedzi OpenTherm (ID 77). Sprawdź połączenie!");
+        // Jeśli nie działa, spróbujmy wysłać pusty status (niezbędny do wybudzenia niektórych Brinków)
+        ot.sendRequest(ot.buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)0, 0));
+        ESP_LOGW("brink", "Brak poprawnej odpowiedzi. Status linii: %d", digitalRead(pin_in));
     }
 
-    // Odczyt temperatury nawiewu (ID 80)
-    unsigned long request80 = ot.buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)80, 0);
-    unsigned long response80 = ot.sendRequest(request80);
-    if (ot.isValidResponse(response80)) {
-        float temp = ot.getFloat(response80);
-        ESP_LOGI("brink", "Temperatura nawiewu: %.1f C", temp);
-        if (supply_temp_sensor != nullptr) supply_temp_sensor->publish_state(temp);
+    // Temperatura (ID 80)
+    response = ot.sendRequest(ot.buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)80, 0));
+    if (ot.isValidResponse(response) && supply_temp_sensor != nullptr) {
+        supply_temp_sensor->publish_state(ot.getFloat(response));
     }
   }
 
   void set_ventilation_level(float level) {
-    ESP_LOGI("brink", "Próba ustawienia mocy na: %.1f%%", level);
     unsigned int data = ot.temperatureToData(level);
-    unsigned long request71 = ot.buildRequest(OpenThermMessageType::WRITE_DATA, (OpenThermMessageID)71, data);
-    unsigned long response = ot.sendRequest(request71);
-    
-    if (ot.isValidResponse(response)) {
-        ESP_LOGI("brink", "Sukces! Brink potwierdził zmianę mocy.");
-    } else {
-        ESP_LOGE("brink", "Błąd! Brink nie odpowiedział na komendę zmiany mocy.");
-    }
+    unsigned long request = ot.buildRequest(OpenThermMessageType::WRITE_DATA, (OpenThermMessageID)71, data);
+    ot.sendRequest(request);
+    ESP_LOGI("brink", "Wysłano żądanie mocy: %.1f", level);
   }
 };
 
