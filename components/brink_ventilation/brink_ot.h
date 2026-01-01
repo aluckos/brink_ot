c93610a64de28243f7dea0dfa058e1d89ca04af6
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-01 21:19:42 +0100

    Update brink_ot.h

--- components/brink_ventilation/brink_ot.h
+++ components/brink_ventilation/brink_ot.h
@@ -7,10 +7,12 @@ namespace esphome {
 namespace brink_ventilation {
 
 class BrinkOpenTherm;
+
+// Globalny wskaźnik dla przerwań (Interrupt Handler)
 static BrinkOpenTherm *global_brink_ot = nullptr;
 static void IRAM_ATTR handleInterrupt();
 
-// Klasa pomocnicza dla suwaka
+// --- Klasa pomocnicza dla Suwaka (Number) ---
 class BrinkNumber : public number::Number {
  public:
   BrinkOpenTherm *parent_;
@@ -18,22 +20,25 @@ class BrinkNumber : public number::Number {
   void control(float value) override;
 };
 
+// --- Główna Klasa Komponentu ---
 class BrinkOpenTherm : public PollingComponent {
  public:
   OpenTherm *ot = nullptr;
   int pin_in, pin_out;
   int current_step = 0;
   float target_ventilation = 0.0f;
-  uint8_t temp_lb = 0;
+  uint8_t temp_lb = 0; // Bufor na młodszy bajt (Low Byte) danych TSP
 
+  // Wskaźniki na sensory (będą ustawione przez Python/to_code)
   sensor::Sensor *t_supply_in_sensor{nullptr};
   sensor::Sensor *t_supply_out_sensor{nullptr};
   sensor::Sensor *t_exhaust_in_sensor{nullptr};
   sensor::Sensor *t_exhaust_out_sensor{nullptr};
   sensor::Sensor *current_flow_sensor{nullptr};
   sensor::Sensor *pressure_in_sensor{nullptr};
-  BrinkNumber *vent_num{nullptr};
+  text_sensor::TextSensor *status_text_sensor{nullptr};
 
+  // Metody ustawiające (wywoływane przez ESPHome przy starcie)
   void set_pins(int in, int out) { pin_in = in; pin_out = out; }
   void set_t_supply_in_sensor(sensor::Sensor *s) { t_supply_in_sensor = s; }
   void set_t_supply_out_sensor(sensor::Sensor *s) { t_supply_out_sensor = s; }
@@ -41,61 +46,20 @@ class BrinkOpenTherm : public PollingComponent {
   void set_t_exhaust_out_sensor(sensor::Sensor *s) { t_exhaust_out_sensor = s; }
   void set_current_flow_sensor(sensor::Sensor *s) { current_flow_sensor = s; }
   void set_pressure_in_sensor(sensor::Sensor *s) { pressure_in_sensor = s; }
-  void set_ventilation_number(BrinkNumber *n) { vent_num = n; }
+  void set_status_text_sensor(text_sensor::TextSensor *s) { status_text_sensor = s; }
+  
+  // Metoda dla suwaka mocy
+  void set_ventilation_number(BrinkNumber *n) { n->set_parent(this); }
 
   void setup() override {
     global_brink_ot = this;
     ot = new OpenTherm(pin_in, pin_out);
     ot->begin(handleInterrupt);
+    ESP_LOGD("brink", "Inicjalizacja OpenTherm na pinach In:%d Out:%d", pin_in, pin_out);
   }
 
   void update() override {
     unsigned long response = 0;
-    ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)0, 0x0100));
-    delay(20);
-
-    switch(current_step) {
-      case 0: 
-        ot->sendRequest(ot->buildRequest(OpenThermMessageType::WRITE_DATA, (OpenThermMessageID)71, (unsigned int)target_ventilation));
-        current_step++; break;
-      case 1:
-        response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)80, 0));
-        if (response && t_supply_in_sensor) t_supply_in_sensor->publish_state(ot->getFloat(response));
-        current_step++; break;
-      case 2:
-        response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)81, 0));
-        if (response && t_supply_out_sensor) t_supply_out_sensor->publish_state(ot->getFloat(response));
-        current_step++; break;
-      case 3:
-        response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)82, 0));
-        if (response && t_exhaust_in_sensor) t_exhaust_in_sensor->publish_state(ot->getFloat(response));
-        current_step++; break;
-      case 4:
-        response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)83, 0));
-        if (response && t_exhaust_out_temp) t_exhaust_out_temp->publish_state(ot->getFloat(response));
-        current_step++; break;
-      case 5: // Flow LB
-        response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)89, 52 << 8));
-        if (response) temp_lb = (uint8_t)(response & 0xFF);
-        current_step++; break;
-      case 6: // Flow HB
-        response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)89, 53 << 8));
-        if (response && current_flow_sensor) current_flow_sensor->publish_state(((uint16_t)(response & 0xFF) << 8) | temp_lb);
-        current_step++; break;
-      case 7: // Pressure LB
-        response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)89, 64 << 8));
-        if (response) temp_lb = (uint8_t)(response & 0xFF);
-        current_step++; break;
-      case 8: // Pressure HB
-        response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)89, 65 << 8));
-        if (response && pressure_in_sensor) pressure_in_sensor->publish_state(((uint16_t)(response & 0xFF) << 8) | temp_lb);
-        current_step = 0; break;
-    }
-  }
-};
-
-inline void BrinkNumber::control(float value) { publish_state(value); parent_->target_ventilation = value; }
-static void IRAM_ATTR handleInterrupt() { if (global_brink_ot) global_brink_ot->ot->handleInterrupt(); }
-
-} // namespace brink_ventilation
-} // namespace esphome
+    
+    // Zawsze wysyłaj Status (ID 0) na początku, aby utrzymać sesję OpenTherm
+    ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)0, 0x
