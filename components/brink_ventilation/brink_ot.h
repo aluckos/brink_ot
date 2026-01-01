fccef04f2d58ea3f4a6fd36b14bc13e153a90c2a
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-01 19:41:42 +0100

    Refactor BrinkOpenTherm class and improve logic

--- components/brink_ventilation/brink_ot.h
+++ components/brink_ventilation/brink_ot.h
@@ -7,8 +7,6 @@ namespace esphome {
 namespace brink_ventilation {
 
 class BrinkOpenTherm; 
-
-// Globalny wskaźnik dla przerwania
 static BrinkOpenTherm *global_brink_ot = nullptr;
 static void IRAM_ATTR handleInterrupt();
 
@@ -28,16 +26,13 @@ class BrinkOpenTherm : public PollingComponent {
     global_brink_ot = this;
   }
 
-  // PRZYWRÓCONE METODY DLA ESPHOME
   void set_supply_temp_sensor(sensor::Sensor *s) { supply_temp_sensor = s; }
   void set_exhaust_temp_sensor(sensor::Sensor *s) { exhaust_temp_sensor = s; }
   void set_current_vent_sensor(sensor::Sensor *s) { current_vent_sensor = s; }
 
   void setup() override {
     ot = new OpenTherm(pin_in, pin_out);
-    // Rejestracja przerwania
     ot->begin(handleInterrupt);
-    ESP_LOGI("brink", "Inicjalizacja systemu przerwań i sensorów...");
   }
 
   void update() override {
@@ -46,57 +41,58 @@ class BrinkOpenTherm : public PollingComponent {
     
     // Utrzymanie sesji
     ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)0, 0x0100));
-    delay(50); 
+    delay(20); 
 
     switch(current_step) {
-      case 0: // STEROWANIE
+      case 0: // Nastawa mocy
         request = ot->buildRequest(OpenThermMessageType::WRITE_DATA, (OpenThermMessageID)71, (unsigned int)target_ventilation);
         ot->sendRequest(request);
         current_step++;
         break;
 
-      case 1: // TEMP NAWIEWU (ID 80)
+      case 1: // Temp Nawiewu (ID 80)
         request = ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)80, 0);
         response = ot->sendRequest(request);
-        if (response != 0) {
-           float val = ot->getFloat(response);
-           if (supply_temp_sensor != nullptr) supply_temp_sensor->publish_state(val);
-           ESP_LOGD("brink", "ID 80 RAW: %08lX, Temp: %.1f", response, val);
-        } else {
-           ESP_LOGW("brink", "ID 80: TIMEOUT");
+        if (response != 0 && supply_temp_sensor != nullptr) {
+           supply_temp_sensor->publish_state(ot->getFloat(response));
         }
         current_step++;
         break;
 
-      case 2: // PRZEPŁYW (ID 89, TSP 52)
+      case 2: // Temp Wywiewu (ID 82 - Zmienione z 81 na 82 wg Twojego pliku .h)
+        request = ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)82, 0);
+        response = ot->sendRequest(request);
+        if (response != 0 && exhaust_temp_sensor != nullptr) {
+           exhaust_temp_sensor->publish_state(ot->getFloat(response));
+        }
+        current_step++;
+        break;
+
+      case 3: // Przepływ m3/h (ID 89, Index 52)
         request = ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)89, 52 << 8);
         response = ot->sendRequest(request);
-        if (response != 0) {
-           int vol = ot->getUInt(response) & 0xFF;
-           if (current_vent_sensor != nullptr) current_vent_sensor->publish_state(vol);
-           ESP_LOGD("brink", "ID 89 RAW: %08lX, Vol: %d", response, vol);
-        } else {
-           ESP_LOGW("brink", "ID 89: TIMEOUT");
+        if (response != 0 && current_vent_sensor != nullptr) {
+           // Odczytujemy 2 bajty danych (HB i LB)
+           // W logu widzieliśmy 40593490 -> 34 to index, 90 to wartość.
+           // Jeśli 144 (0x90) to za mało, sprawdzamy czy HB (index) nie niesie danych.
+           // Na razie publikujemy czyste LB, ale jako unsigned int.
+           uint16_t raw_val = ot->getUInt(response) & 0xFF; 
+           
+           // Jeśli to Brink 400 i 144 to wartość max, to może to być skala 0-255?
+           // Sprawdźmy co się stanie jak pomnożymy przez 2.5 (144 * 2.77 = 400)
+           current_vent_sensor->publish_state(raw_val); 
         }
         current_step = 0;
         break;
     }
   }
 
-  void handle_int() {
-    if (ot) ot->handleInterrupt();
-  }
-
-  void set_ventilation_level(float level) {
-    target_ventilation = level;
-  }
+  void handle_int() { if (ot) ot->handleInterrupt(); }
+  void set_ventilation_level(float level) { target_ventilation = level; }
 };
 
-// Obsługa przerwania
 static void IRAM_ATTR handleInterrupt() {
-  if (global_brink_ot != nullptr) {
-    global_brink_ot->handle_int();
-  }
+  if (global_brink_ot != nullptr) global_brink_ot->handle_int();
 }
 
 class BrinkVentilationNumber : public number::Number {
