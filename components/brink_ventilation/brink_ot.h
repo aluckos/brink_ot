52c6d53c78463573a6a3d24f7455ac1e68acbc6c
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-01 17:21:55 +0100

    Refactor BrinkOpenTherm class and update methods

--- components/brink_ventilation/brink_ot.h
+++ components/brink_ventilation/brink_ot.h
@@ -1,50 +1,62 @@
+#pragma once
+
 #include "esphome.h"
-#include <OpenTherm.h> // Zakładając, że biblioteka jest dostępna w frameworku
+#include <OpenTherm.h>
 
-class BrinkOpenTherm : public PollingComponent, public CustomAPIDevice {
+using namespace esphome;
+
+class BrinkOpenTherm : public PollingComponent {
  public:
-  // Sensory dla Home Assistant
-  Sensor *current_vent_sensor = new Sensor();
-  Sensor *supply_temp_sensor = new Sensor();
-  Sensor *exhaust_temp_sensor = new Sensor();
-
   OpenTherm ot;
+  int in_pin, out_pin;
 
-  BrinkOpenTherm(int in_pin, int out_pin) : PollingComponent(5000) {
-    ot.begin(in_pin, out_pin);
-  }
+  // Sensory i Sterowanie
+  sensor::Sensor *current_vent_sensor{nullptr};
+  sensor::Sensor *supply_temp_sensor{nullptr};
+  sensor::Sensor *exhaust_temp_sensor{nullptr};
+  
+  BrinkOpenTherm(int in, int out) : PollingComponent(10000), in_pin(in), out_pin(out) {}
 
   void setup() override {
-    // Rejestracja funkcji sterującej, którą wywołamy z YAML
-    register_service(&BrinkOpenTherm::set_ventilation, "set_ventilation", {"level"});
+    ot.begin(in_pin, out_pin);
+    ESP_LOGCONFIG("brink", "OpenTherm setup na pinach IN:%d, OUT:%d", in_pin, out_pin);
   }
 
+  // Settery dla Pythona
+  void set_current_vent_sensor(sensor::Sensor *s) { current_vent_sensor = s; }
+  void set_supply_temp_sensor(sensor::Sensor *s) { supply_temp_sensor = s; }
+  void set_exhaust_temp_sensor(sensor::Sensor *s) { exhaust_temp_sensor = s; }
+
   void update() override {
-    // Odczyt aktualnej prędkości (ID 77)
+    // ID 77: Aktualna moc wentylacji (Relative ventilation)
     unsigned long response = ot.sendRequest(ot.buildRequest(OpenThermMessageType::Read_Data, 77, 0));
-    if (ot.isValidResponse(response)) {
-        float value = ot.getUInt(response) / 1.0f; // Brink często zwraca % jako uint
-        current_vent_sensor->publish_state(value);
+    if (ot.isValidResponse(response) && current_vent_sensor != nullptr) {
+        current_vent_sensor->publish_state(ot.getFloat(response));
     }
 
-    // Odczyt temp nawiewu (ID 80)
+    // ID 80: Temperatura nawiewu
     response = ot.sendRequest(ot.buildRequest(OpenThermMessageType::Read_Data, 80, 0));
-    if (ot.isValidResponse(response)) {
+    if (ot.isValidResponse(response) && supply_temp_sensor != nullptr) {
         supply_temp_sensor->publish_state(ot.getFloat(response));
     }
-    
-    // Odczyt temp wywiewu (ID 82)
+
+    // ID 82: Temperatura wywiewu
     response = ot.sendRequest(ot.buildRequest(OpenThermMessageType::Read_Data, 82, 0));
-    if (ot.isValidResponse(response)) {
+    if (ot.isValidResponse(response) && exhaust_temp_sensor != nullptr) {
         exhaust_temp_sensor->publish_state(ot.getFloat(response));
     }
   }
 
-  void set_ventilation(float level) {
-    // Wysyłamy ID 71 (Set Ventilation Level)
-    // Format f8.8: mnożymy przez 256
-    uint16_t data = (uint16_t)(level * 256.0f / 100.0f);
-    ot.sendRequest(ot.buildRequest(OpenThermMessageType::Write_Data, 71, data));
-    ESP_LOGD("brink", "Ustawiono wentylację na: %.1f%%", level);
+  void set_ventilation_level(float level) {
+    // ID 71: Sterowanie (Set Ventilation Level)
+    // Konwersja na format f8.8 (wartość * 256)
+    uint16_t data = ot.temperatureToData(level); 
+    unsigned long response = ot.sendRequest(ot.buildRequest(OpenThermMessageType::Write_Data, 71, data));
+    
+    if (ot.isValidResponse(response)) {
+        ESP_LOGD("brink", "Ustawiono wentylację na %.1f%%", level);
+    } else {
+        ESP_LOGE("brink", "Błąd zapisu wentylacji!");
+    }
   }
 };
