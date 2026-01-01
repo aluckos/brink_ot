70ca17880a2bbd86faa76d14dd3441fe14394f3b
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-01 17:25:13 +0100

    Update polling interval and add configuration constant

--- components/brink_ventilation/__init__.py
+++ components/brink_ventilation/__init__.py
@@ -1,20 +1,19 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart
 from esphome.const import CONF_ID
 
-# Definicja namespace i klasy
 brink_ns = cg.esphome_ns.namespace('brink_ventilation')
 BrinkOpenTherm = brink_ns.class_('BrinkOpenTherm', cg.PollingComponent)
+
+CONF_BRINK_VENTILATION_ID = "brink_ventilation_id"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(BrinkOpenTherm),
     cv.Required("in_pin"): cv.int_,
     cv.Required("out_pin"): cv.int_,
-}).extend(cv.polling_component_schema('60s'))
+}).extend(cv.polling_component_schema('10s'))
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID], config['in_pin'], config['out_pin'])
     yield cg.register_component(var, config)
-    # Dodanie biblioteki zewnętrznej
     cg.add_library("ihormaze/OpenTherm Library", "1.1.5")
