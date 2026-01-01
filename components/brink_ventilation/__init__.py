8116ee4c90ad45560a586164203cced856bdf434
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-01 17:40:01 +0100

    Change library registration to include name 'OpenTherm'
    Updated library registration to include a name.

--- components/brink_ventilation/__init__.py
+++ components/brink_ventilation/__init__.py
@@ -18,5 +18,5 @@ CONFIG_SCHEMA = cv.Schema({
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID], config['in_pin'], config['out_pin'])
     yield cg.register_component(var, config)
-    # Pobieranie biblioteki jako ZIP - omija błędy 'git clone' i 'Username for github'
-    cg.add_library(None, None, "https://github.com/ihormaze/OpenTherm/archive/refs/heads/master.zip")
+    # Dodajemy nazwę 'OpenTherm', wersję None i link do ZIPa
+    cg.add_library("OpenTherm", None, "https://github.com/ihormaze/OpenTherm/archive/refs/heads/master.zip")
