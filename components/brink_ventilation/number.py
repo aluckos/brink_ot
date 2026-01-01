1481dd8f5c49ffab89183f4669ba76a5577bc442
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-01 21:13:50 +0100

    Update number.py

--- components/brink_ventilation/number.py
+++ components/brink_ventilation/number.py
@@ -1,19 +1,16 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import number
-from esphome.const import CONF_ID, UNIT_PERCENT, ICON_FAN
-from . import brink_ns, BrinkOpenTherm, CONF_BRINK_VENTILATION_ID
+from esphome.const import CONF_ID, CONF_TYPE
+from . import BRINK_VENTILATION_ID, BrinkOpenTherm
 
-BrinkVentilationNumber = brink_ns.class_("BrinkVentilationNumber", number.Number)
-
-# Używamy funkcji number_schema zamiast stałej NUMBER_SCHEMA
-CONFIG_SCHEMA = number.number_schema(
-    BrinkVentilationNumber,
-).extend({
-    cv.GenerateID(CONF_BRINK_VENTILATION_ID): cv.use_id(BrinkOpenTherm),
+CONFIG_SCHEMA = number.NUMBER_SCHEMA.extend({
+    cv.GenerateID(BRINK_VENTILATION_ID): cv.use_id(BrinkOpenTherm),
 }).extend(cv.COMPONENT_SCHEMA)
 
-def to_code(config):
-    paren = yield cg.get_variable(config[CONF_BRINK_VENTILATION_ID])
-    var = yield number.new_number(config, min_value=0, max_value=100, step=1)
-    cg.add(var.set_parent(paren))
+async def to_code(config):
+    parent = await cg.get_variable(config[BRINK_VENTILATION_ID])
+    # Tworzymy pomocniczą klasę w C++ dla suwaka
+    var = await number.new_number(config, min_value=0, max_value=100, step=1)
+    cg.add(var.set_parent(parent))
+    cg.add(parent.set_ventilation_number(var))
