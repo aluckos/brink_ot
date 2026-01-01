b67d1e844cbd2db8bb4a0fcc0c76a6d8142ede4e
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-01 21:13:08 +0100

    Update __init__.py

--- components/brink_ventilation/__init__.py
+++ components/brink_ventilation/__init__.py
@@ -1,22 +1,21 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome import pins
 from esphome.const import CONF_ID
 
-# Definicja przestrzeni nazw
-brink_ns = cg.esphome_ns.namespace('brink_ventilation')
-BrinkOpenTherm = brink_ns.class_('BrinkOpenTherm', cg.PollingComponent)
+# To musi pasować do Twojego namespace w C++
+brink_ventilation_ns = cg.esphome_ns.namespace("brink_ventilation")
+BrinkOpenTherm = brink_ventilation_ns.class_("BrinkOpenTherm", cg.PollingComponent)
 
-# Stała współdzielona między plikami
-CONF_BRINK_VENTILATION_ID = "brink_ventilation_id"
+BRINK_VENTILATION_ID = "brink_ventilation_id"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(BrinkOpenTherm),
-    cv.Required("in_pin"): cv.int_,
-    cv.Required("out_pin"): cv.int_,
-}).extend(cv.polling_component_schema('10s'))
+    cv.Required("in_pin"): pins.gpio_input_pin_number,
+    cv.Required("out_pin"): pins.gpio_output_pin_number,
+}).extend(cv.polling_component_schema("1500ms"))
 
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID], config['in_pin'], config['out_pin'])
-    yield cg.register_component(var, config)
-    # Zmieniona metoda dodawania biblioteki - wskazujemy konkretne repozytorium Ihora Melnyka
-    cg.add_library("OpenTherm", None, "https://github.com/ihormelnyk/opentherm_library.git")
+async def to_code(config):
+    var = cg.new_variable(config[CONF_ID])
+    await cg.register_component(var, config)
+    cg.add(var.set_pins(config["in_pin"], config["out_pin"]))
