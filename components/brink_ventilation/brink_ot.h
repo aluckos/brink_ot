6b525b14afd98a9480e166e6eeedd68c06637033
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-01 21:14:11 +0100

    Update brink_ot.h

--- components/brink_ventilation/brink_ot.h
+++ components/brink_ventilation/brink_ot.h
@@ -6,9 +6,17 @@
 namespace esphome {
 namespace brink_ventilation {
 
-class BrinkOpenTherm; 
+class BrinkOpenTherm;
 static BrinkOpenTherm *global_brink_ot = nullptr;
 static void IRAM_ATTR handleInterrupt();
+
+// Klasa pomocnicza dla suwaka
+class BrinkNumber : public number::Number {
+ public:
+  BrinkOpenTherm *parent_;
+  void set_parent(BrinkOpenTherm *parent) { parent_ = parent; }
+  void control(float value) override;
+};
 
 class BrinkOpenTherm : public PollingComponent {
  public:
@@ -16,92 +24,78 @@ class BrinkOpenTherm : public PollingComponent {
   int pin_in, pin_out;
   int current_step = 0;
   float target_ventilation = 0.0f;
-  uint8_t temp_lb = 0; 
+  uint8_t temp_lb = 0;
 
-  sensor::Sensor *supply_in_temp = new sensor::Sensor();
-  sensor::Sensor *supply_out_temp = new sensor::Sensor();
-  sensor::Sensor *exhaust_in_temp = new sensor::Sensor();
-  sensor::Sensor *exhaust_out_temp = new sensor::Sensor();
-  sensor::Sensor *current_flow = new sensor::Sensor();
-  sensor::Sensor *pressure_in = new sensor::Sensor();
+  sensor::Sensor *t_supply_in_sensor{nullptr};
+  sensor::Sensor *t_supply_out_sensor{nullptr};
+  sensor::Sensor *t_exhaust_in_sensor{nullptr};
+  sensor::Sensor *t_exhaust_out_sensor{nullptr};
+  sensor::Sensor *current_flow_sensor{nullptr};
+  sensor::Sensor *pressure_in_sensor{nullptr};
+  BrinkNumber *vent_num{nullptr};
 
-  BrinkOpenTherm(int in, int out) : PollingComponent(1500), pin_in(in), pin_out(out) {
-    global_brink_ot = this;
-  }
+  void set_pins(int in, int out) { pin_in = in; pin_out = out; }
+  void set_t_supply_in_sensor(sensor::Sensor *s) { t_supply_in_sensor = s; }
+  void set_t_supply_out_sensor(sensor::Sensor *s) { t_supply_out_sensor = s; }
+  void set_t_exhaust_in_sensor(sensor::Sensor *s) { t_exhaust_in_sensor = s; }
+  void set_t_exhaust_out_sensor(sensor::Sensor *s) { t_exhaust_out_sensor = s; }
+  void set_current_flow_sensor(sensor::Sensor *s) { current_flow_sensor = s; }
+  void set_pressure_in_sensor(sensor::Sensor *s) { pressure_in_sensor = s; }
+  void set_ventilation_number(BrinkNumber *n) { vent_num = n; }
 
   void setup() override {
+    global_brink_ot = this;
     ot = new OpenTherm(pin_in, pin_out);
     ot->begin(handleInterrupt);
   }
 
   void update() override {
     unsigned long response = 0;
-    // Status ID 0 dla utrzymania połączenia
     ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)0, 0x0100));
     delay(20);
 
     switch(current_step) {
-      case 0: // Nastawa mocy
+      case 0: 
         ot->sendRequest(ot->buildRequest(OpenThermMessageType::WRITE_DATA, (OpenThermMessageID)71, (unsigned int)target_ventilation));
-        current_step++;
-        break;
-      case 1: // T1 (ID 80)
+        current_step++; break;
+      case 1:
         response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)80, 0));
-        if (response) supply_in_temp->publish_state(ot->getFloat(response));
-        current_step++;
-        break;
-      case 2: // T2 (ID 81)
+        if (response && t_supply_in_sensor) t_supply_in_sensor->publish_state(ot->getFloat(response));
+        current_step++; break;
+      case 2:
         response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)81, 0));
-        if (response) supply_out_temp->publish_state(ot->getFloat(response));
-        current_step++;
-        break;
-      case 3: // T3 (ID 82)
+        if (response && t_supply_out_sensor) t_supply_out_sensor->publish_state(ot->getFloat(response));
+        current_step++; break;
+      case 3:
         response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)82, 0));
-        if (response) exhaust_in_temp->publish_state(ot->getFloat(response));
-        current_step++;
-        break;
-      case 4: // T4 (ID 83)
+        if (response && t_exhaust_in_sensor) t_exhaust_in_sensor->publish_state(ot->getFloat(response));
+        current_step++; break;
+      case 4:
         response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)83, 0));
-        if (response) exhaust_out_temp->publish_state(ot->getFloat(response));
-        current_step++;
-        break;
-      case 5: // Przepływ LB (TSP 52)
+        if (response && t_exhaust_out_temp) t_exhaust_out_temp->publish_state(ot->getFloat(response));
+        current_step++; break;
+      case 5: // Flow LB
         response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)89, 52 << 8));
         if (response) temp_lb = (uint8_t)(response & 0xFF);
-        current_step++;
-        break;
-      case 6: // Przepływ HB (TSP 53)
+        current_step++; break;
+      case 6: // Flow HB
         response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)89, 53 << 8));
-        if (response) current_flow->publish_state(((uint16_t)(response & 0xFF) << 8) | temp_lb);
-        current_step++;
-        break;
-      case 7: // Ciśnienie LB (TSP 64)
+        if (response && current_flow_sensor) current_flow_sensor->publish_state(((uint16_t)(response & 0xFF) << 8) | temp_lb);
+        current_step++; break;
+      case 7: // Pressure LB
         response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)89, 64 << 8));
         if (response) temp_lb = (uint8_t)(response & 0xFF);
-        current_step++;
-        break;
-      case 8: // Ciśnienie HB (TSP 65)
+        current_step++; break;
+      case 8: // Pressure HB
         response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)89, 65 << 8));
-        if (response) pressure_in->publish_state(((uint16_t)(response & 0xFF) << 8) | temp_lb);
-        current_step = 0;
-        break;
+        if (response && pressure_in_sensor) pressure_in_sensor->publish_state(((uint16_t)(response & 0xFF) << 8) | temp_lb);
+        current_step = 0; break;
     }
   }
-
-  void handle_int() { if (ot) ot->handleInterrupt(); }
 };
 
-static void IRAM_ATTR handleInterrupt() { if (global_brink_ot) global_brink_ot->handle_int(); }
-
-class BrinkVentilationNumber : public number::Number {
- public:
-  BrinkOpenTherm *parent_;
-  void set_parent(BrinkOpenTherm *parent) { parent_ = parent; }
-  void control(float value) override {
-    this->publish_state(value);
-    parent_->target_ventilation = value;
-  }
-};
+inline void BrinkNumber::control(float value) { publish_state(value); parent_->target_ventilation = value; }
+static void IRAM_ATTR handleInterrupt() { if (global_brink_ot) global_brink_ot->ot->handleInterrupt(); }
 
 } // namespace brink_ventilation
 } // namespace esphome
