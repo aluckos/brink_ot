af70e5eea3112c0a183cfd4dc345e62ebe0db1a0
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-01 17:32:16 +0100

    Update CONFIG_SCHEMA to use number_schema

--- components/brink_ventilation/number.py
+++ components/brink_ventilation/number.py
@@ -6,13 +6,14 @@ from . import brink_ns, BrinkOpenTherm, CONF_BRINK_VENTILATION_ID
 
 BrinkVentilationNumber = brink_ns.class_("BrinkVentilationNumber", number.Number)
 
-CONFIG_SCHEMA = number.NUMBER_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(BrinkVentilationNumber),
+# Zmieniono number.NUMBER_SCHEMA na number.number_schema
+CONFIG_SCHEMA = number.number_schema(
+    BrinkVentilationNumber,
+).extend({
     cv.GenerateID(CONF_BRINK_VENTILATION_ID): cv.use_id(BrinkOpenTherm),
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
     paren = yield cg.get_variable(config[CONF_BRINK_VENTILATION_ID])
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield number.register_number(var, config, min_value=0, max_value=100, step=1)
+    var = yield number.new_number(config, min_value=0, max_value=100, step=1)
     cg.add(var.set_parent(paren))
