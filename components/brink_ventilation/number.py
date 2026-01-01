2407fce645d82363f0cee52884eb980005f6746e
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-01 17:22:22 +0100

    Add BrinkVentilationNumber component
    Implement BrinkVentilationNumber component with configuration and code generation.

---
+++ components/brink_ventilation/number.py
@@ -0,0 +1,20 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import number
+from esphome.const import CONF_ID, UNIT_PERCENT, ICON_FAN
+from . import brink_ns, BrinkOpenTherm, CONF_BRINK_VENTILATION_ID
+
+BrinkVentilationNumber = brink_ns.class_("BrinkVentilationNumber", number.Number)
+
+CONFIG_SCHEMA = number.NUMBER_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(BrinkVentilationNumber),
+    cv.GenerateID(CONF_BRINK_VENTILATION_ID): cv.use_id(BrinkOpenTherm),
+}).extend(cv.COMPONENT_SCHEMA)
+
+def to_code(config):
+    paren = yield cg.get_variable(config[CONF_BRINK_VENTILATION_ID])
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield number.register_number(var, config, min_value=0, max_value=100, step=1)
+    
+    # Połącz suwak z funkcją w C++
+    cg.add(var.set_parent(paren))
