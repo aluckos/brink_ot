84ff2a505d6c0e72febfdfbd2d0a85003d8ea4fe
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-01 17:49:46 +0100

    Refactor OpenTherm includes and cleanup code

--- components/brink_ventilation/brink_ot.h
+++ components/brink_ventilation/brink_ot.h
@@ -1,15 +1,14 @@
 #pragma once
 
 #include "esphome.h"
-#include <opentherm.h>
+#include "OpenTherm.h" // Zmienione z opentherm.h na OpenTherm.h
 
 namespace esphome {
 namespace brink_ventilation {
 
-// Definicja statycznych zmiennych dla obsługi przerwań biblioteki
-static int global_in_pin;
+// Definicja statycznych zmiennych dla obsługi przerwań
 static void IRAM_ATTR handleInterrupt() {
-    // Pusta funkcja dla biblioteki, aby kompilator nie zgłaszał błędów
+    // Pusta funkcja
 }
 
 class BrinkOpenTherm : public PollingComponent {
@@ -21,14 +20,11 @@ class BrinkOpenTherm : public PollingComponent {
   sensor::Sensor *supply_temp_sensor{nullptr};
   sensor::Sensor *exhaust_temp_sensor{nullptr};
   
-  BrinkOpenTherm(int in, int out) : PollingComponent(10000), in_pin(in), out_pin(out) {
-    global_in_pin = in;
-  }
+  BrinkOpenTherm(int in, int out) : PollingComponent(10000), in_pin(in), out_pin(out) {}
 
   void setup() override {
-    // Inicjalizacja biblioteki Ihora Melnyka z podaniem pinów i funkcji przerwania
+    // Inicjalizacja biblioteki
     ot.begin(in_pin, out_pin, handleInterrupt);
-    ESP_LOGD("brink", "OpenTherm zainicjalizowany");
   }
 
   void set_current_vent_sensor(sensor::Sensor *s) { current_vent_sensor = s; }
@@ -36,34 +32,28 @@ class BrinkOpenTherm : public PollingComponent {
   void set_exhaust_temp_sensor(sensor::Sensor *s) { exhaust_temp_sensor = s; }
 
   void update() override {
-    // ID 77: Relative ventilation (%)
-    unsigned long request77 = ot.buildRequest(OpenThermMessageType::Read_Data, 77, 0);
-    unsigned long response77 = ot.sendRequest(request77);
-    if (ot.isValidResponse(response77) && current_vent_sensor != nullptr) {
-        current_vent_sensor->publish_state(ot.getUInt(response77));
+    // ID 77: Relative ventilation
+    unsigned long response = ot.sendRequest(ot.buildRequest(OpenThermMessageType::Read_Data, 77, 0));
+    if (ot.isValidResponse(response) && current_vent_sensor != nullptr) {
+        current_vent_sensor->publish_state(ot.getUInt(response));
     }
 
     // ID 80: Supply inlet temp
-    unsigned long request80 = ot.buildRequest(OpenThermMessageType::Read_Data, 80, 0);
-    unsigned long response80 = ot.sendRequest(request80);
-    if (ot.isValidResponse(response80) && supply_temp_sensor != nullptr) {
-        supply_temp_sensor->publish_state(ot.getFloat(response80));
+    response = ot.sendRequest(ot.buildRequest(OpenThermMessageType::Read_Data, 80, 0));
+    if (ot.isValidResponse(response) && supply_temp_sensor != nullptr) {
+        supply_temp_sensor->publish_state(ot.getFloat(response));
     }
 
     // ID 82: Exhaust air temp
-    unsigned long request82 = ot.buildRequest(OpenThermMessageType::Read_Data, 82, 0);
-    unsigned long response82 = ot.sendRequest(request82);
-    if (ot.isValidResponse(response82) && exhaust_temp_sensor != nullptr) {
-        exhaust_temp_sensor->publish_state(ot.getFloat(response82));
+    response = ot.sendRequest(ot.buildRequest(OpenThermMessageType::Read_Data, 82, 0));
+    if (ot.isValidResponse(response) && exhaust_temp_sensor != nullptr) {
+        exhaust_temp_sensor->publish_state(ot.getFloat(response));
     }
   }
 
   void set_ventilation_level(float level) {
-    // W tej bibliotece temperatureToData przelicza float na format f8.8 (używany też dla %)
     unsigned int data = ot.temperatureToData(level);
-    unsigned long request71 = ot.buildRequest(OpenThermMessageType::Write_Data, 71, data);
-    ot.sendRequest(request71);
-    ESP_LOGD("brink", "Wysłano żądanie ustawienia wentylacji: %.1f%%", level);
+    ot.sendRequest(ot.buildRequest(OpenThermMessageType::Write_Data, 71, data));
   }
 };
 
