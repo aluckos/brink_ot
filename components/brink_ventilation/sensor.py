85bf5e3096f976a9f0a7e6e6beaaee5fd0ca364d
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-01 21:35:11 +0100

    Update sensor.py

--- components/brink_ventilation/sensor.py
+++ components/brink_ventilation/sensor.py
@@ -7,7 +7,6 @@ from esphome.const import (
 )
 from . import BRINK_VENTILATION_ID, BrinkOpenTherm
 
-# Definicje danych dla poszczególnych sensorów
 TYPES = {
     "T_SUPPLY_IN": ["Brink Temp Czerpnia", UNIT_CELSIUS, 1, DEVICE_CLASS_TEMPERATURE],
     "T_SUPPLY_OUT": ["Brink Temp Nawiew", UNIT_CELSIUS, 1, DEVICE_CLASS_TEMPERATURE],
@@ -24,25 +23,20 @@ CONFIG_SCHEMA = cv.Schema({
 
 async def to_code(config):
     parent = await cg.get_variable(config[BRINK_VENTILATION_ID])
-    
-    # Pobieramy dane domyślne dla wybranego typu
     conf_data = TYPES[config[CONF_TYPE]]
     
-    # Tworzymy kopię konfiguracji i uzupełniamy o brakujące parametry, 
-    # jeśli użytkownik nie podał ich w YAML
     sensor_config = config.copy()
     if CONF_NAME not in sensor_config:
         sensor_config[CONF_NAME] = conf_data[0]
     
-    # Tworzymy obiekt sensora korzystając z wbudowanej funkcji ESPHome
     var = await sensor.new_sensor(sensor_config)
     
-    # Ustawiamy parametry, których new_sensor nie ustawia automatycznie z naszej mapy TYPES
     cg.add(var.set_unit_of_measurement(conf_data[1]))
     cg.add(var.set_accuracy_decimals(conf_data[2]))
     if conf_data[3] is not None:
         cg.add(var.set_device_class(conf_data[3]))
+    
+    # KLUCZOWA ZMIANA: Poprawne ustawienie StateClass dla C++
     cg.add(var.set_state_class(STATE_CLASS_MEASUREMENT))
     
-    # Łączymy sensor z odpowiednią metodą w pliku brink_ot.h
     cg.add(getattr(parent, f"set_{config[CONF_TYPE].lower()}_sensor")(var))
