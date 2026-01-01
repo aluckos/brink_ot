fc7f61633060757c943b5259724c0986e7ed2e54
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-01 21:20:35 +0100

    Update __init__.py

--- components/brink_ventilation/__init__.py
+++ components/brink_ventilation/__init__.py
@@ -1,9 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import pins
 from esphome.const import CONF_ID
 
-# To musi pasować do Twojego namespace w C++
 brink_ventilation_ns = cg.esphome_ns.namespace("brink_ventilation")
 BrinkOpenTherm = brink_ventilation_ns.class_("BrinkOpenTherm", cg.PollingComponent)
 
@@ -11,7 +9,6 @@ BRINK_VENTILATION_ID = "brink_ventilation_id"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(BrinkOpenTherm),
-    # Używamy prostego int dla numeru pinu, co jest najbezpieczniejsze
     cv.Required("in_pin"): cv.int_,
     cv.Required("out_pin"): cv.int_,
 }).extend(cv.polling_component_schema("1500ms"))
@@ -19,5 +16,4 @@ CONFIG_SCHEMA = cv.Schema({
 async def to_code(config):
     var = cg.new_variable(config[CONF_ID])
     await cg.register_component(var, config)
-    # Przekazujemy numery pinów do funkcji set_pins w C++
     cg.add(var.set_pins(config["in_pin"], config["out_pin"]))
