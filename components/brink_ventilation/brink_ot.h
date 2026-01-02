512f3cf080e4dc7c924e13c359fb9df459cd4e89
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-02 01:34:58 +0100

    Update brink_ot.h

--- components/brink_ventilation/brink_ot.h
+++ components/brink_ventilation/brink_ot.h
@@ -24,13 +24,14 @@ class BrinkOpenTherm : public PollingComponent {
   int pin_in, pin_out;
   int current_step = 0;
   float target_ventilation = 25.0f;
+  uint8_t temp_lb = 0;
 
   sensor::Sensor *t_supply_in_sensor{nullptr};
   sensor::Sensor *t_supply_out_sensor{nullptr};
   sensor::Sensor *t_exhaust_in_sensor{nullptr};
   sensor::Sensor *t_exhaust_out_sensor{nullptr};
   sensor::Sensor *current_flow_sensor{nullptr};
-  sensor::Sensor *pressure_in_sensor{nullptr};
+  sensor::Sensor *pressure_in_sensor{nullptr}; // Tu będziemy wysyłać RPM dla testu
   text_sensor::TextSensor *status_text_sensor{nullptr};
 
   void set_pins(int in, int out) { pin_in = in; pin_out = out; }
@@ -51,41 +52,55 @@ class BrinkOpenTherm : public PollingComponent {
 
   void update() override {
     unsigned long response = 0;
-    // Podtrzymanie komunikacji
     ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)0, 0x0100));
     delay(20);
 
+    if (this->status_text_sensor != nullptr) {
+      this->status_text_sensor->publish_state("Połączono");
+    }
+
     switch(current_step) {
-      case 0: // Nastawa (ID 71)
+      case 0:
         ot->sendRequest(ot->buildRequest(OpenThermMessageType::WRITE_DATA, (OpenThermMessageID)71, (unsigned int)target_ventilation));
         current_step++; break;
-
-      case 1: // T1 (ID 80)
+      case 1:
         response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)80, 0));
         if (response && t_supply_in_sensor) t_supply_in_sensor->publish_state(ot->getFloat(response));
         current_step++; break;
-      case 2: // RPM Nawiew (ID 85 z Twojej listy) - mapujemy na T2 dla testu
-        response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)85, 0));
-        if (response && t_supply_out_sensor) t_supply_out_sensor->publish_state((float)ot->getUInt(response));
+      case 2: // T2 - Test TSP 45 (często T_supply_out we Flair)
+        response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)89, 45 << 8));
+        if (response && t_supply_out_sensor) {
+          t_supply_out_sensor->publish_state((float)(response & 0xFF));
+        }
         current_step++; break;
-      case 3: // T3 (ID 82)
+      case 3:
         response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)82, 0));
         if (response && t_exhaust_in_sensor) t_exhaust_in_sensor->publish_state(ot->getFloat(response));
         current_step++; break;
-      case 4: // RPM Wywiew (ID 84 z Twojej listy) - mapujemy na T4 dla testu
-        response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)84, 0));
-        if (response && t_exhaust_out_sensor) t_exhaust_out_sensor->publish_state((float)ot->getUInt(response));
+      case 4: // T4 - Test TSP 46 (często T_exhaust_out we Flair)
+        response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)89, 46 << 8));
+        if (response && t_exhaust_out_sensor) {
+          t_exhaust_out_sensor->publish_state((float)(response & 0xFF));
+        }
         current_step++; break;
-      case 5: // PRZEPŁYW LB (TSP 52)
+      case 5:
         response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)89, 52 << 8));
         if (response) temp_lb = (uint8_t)(response & 0xFF);
         current_step++; break;
-      case 6: // PRZEPŁYW HB (TSP 53)
+      case 6:
         response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)89, 53 << 8));
         if (response && current_flow_sensor) {
           current_flow_sensor->publish_state(((uint16_t)(response & 0xFF) << 8) | temp_lb);
         }
-        current_step++; break;    }
+        current_step++; break;
+      case 7: // Zamiast Ciśnienia - odczytujemy RPM wentylatora nawiewnego (ID 85)
+        response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)85, 0));
+        if (response && pressure_in_sensor) {
+          // Używamy sensora ciśnienia jako wyświetlacza RPM dla testu
+          pressure_in_sensor->publish_state((float)ot->getUInt(response));
+        }
+        current_step = 0; break;
+    }
   }
 };
 
