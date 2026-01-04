6bbd3a0521691b3d80077a676be66428c8ff1d70
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-04 09:30:54 +0100

    Update text_sensor.py

--- components/brink_ventilation/text_sensor.py
+++ components/brink_ventilation/text_sensor.py
@@ -4,7 +4,11 @@ from esphome.components import text_sensor
 from esphome.const import CONF_ID, CONF_TYPE
 from . import BRINK_VENTILATION_ID, BrinkOpenTherm
 
-TYPES = {"STATUS": "Brink Status Pracy"}
+# Dodajemy CURRENT_GEAR do dostępnych typów
+TYPES = {
+    "STATUS": "Brink Status Pracy",
+    "CURRENT_GEAR": "Brink Aktualny Bieg"
+}
 
 CONFIG_SCHEMA = text_sensor.text_sensor_schema().extend({
     cv.GenerateID(BRINK_VENTILATION_ID): cv.use_id(BrinkOpenTherm),
@@ -14,4 +18,9 @@ CONFIG_SCHEMA = text_sensor.text_sensor_schema().extend({
 async def to_code(config):
     parent = await cg.get_variable(config[BRINK_VENTILATION_ID])
     var = await text_sensor.new_text_sensor(config)
-    cg.add(parent.set_status_text_sensor(var))
+    
+    # Dynamiczne budowanie nazwy funkcji, np. set_status_sensor lub set_current_gear_sensor
+    # Uwaga: w brink_ot.h zmieniłem set_status_text_sensor na set_status_sensor dla spójności
+    func_name = f"set_{config[CONF_TYPE].lower()}_sensor"
+    func = getattr(parent, func_name)
+    cg.add(func(var))
