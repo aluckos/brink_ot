3223edb06174c4ca308f1a4260f163ca28d15aeb
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-04 10:50:42 +0100

    Update brink_ot.h

--- components/brink_ventilation/brink_ot.h
+++ components/brink_ventilation/brink_ot.h
@@ -10,7 +10,6 @@ static const char *const TAG = "brink_ot";
 
 class BrinkOpenTherm;
 
-// Klasa do obsługi suwaka/nastawy
 class BrinkNumber : public number::Number {
  public:
   BrinkOpenTherm *parent_{nullptr};
@@ -23,23 +22,20 @@ class BrinkOpenTherm : public PollingComponent {
   OpenTherm *ot{nullptr};
   int pin_in, pin_out;
   int current_step = 0;
-  float target_ventilation = 25.0f; // To steruje % lub m3/h
-  uint8_t temp_lb = 0;
+  float target_ventilation = 25.0f;
 
-  // Sensory temperatury (TSP zgodne z Twoją listą)
-  sensor::Sensor *t_supply_in_sensor{nullptr};   // T1 (TSP 57)
-  sensor::Sensor *t_supply_out_sensor{nullptr};  // T2 (TSP 69)
-  sensor::Sensor *t_exhaust_in_sensor{nullptr};  // T3 (TSP 58)
-  sensor::Sensor *t_exhaust_out_sensor{nullptr}; // T4 (TSP 70)
-  sensor::Sensor *current_flow_sensor{nullptr};  // Przepływ (TSP 62)
+  sensor::Sensor *t_supply_in_sensor{nullptr};   
+  sensor::Sensor *t_supply_out_sensor{nullptr};  
+  sensor::Sensor *t_exhaust_in_sensor{nullptr};  
+  sensor::Sensor *t_exhaust_out_sensor{nullptr}; 
+  sensor::Sensor *current_flow_sensor{nullptr};  
   
-  binary_sensor::BinarySensor *filter_status_binary{nullptr}; // Filtr (TSP 13)
+  binary_sensor::BinarySensor *filter_status_binary{nullptr}; 
   text_sensor::TextSensor *status_sensor{nullptr};
   text_sensor::TextSensor *current_gear_sensor{nullptr};
 
   void set_pins(int in, int out) { pin_in = in; pin_out = out; }
   
-  // Settery wywoływane przez kod generowany przez ESPHome
   void set_t_supply_in_sensor(sensor::Sensor *s) { t_supply_in_sensor = s; }
   void set_t_supply_out_sensor(sensor::Sensor *s) { t_supply_out_sensor = s; } 
   void set_t_exhaust_in_sensor(sensor::Sensor *s) { t_exhaust_in_sensor = s; }
@@ -54,7 +50,6 @@ class BrinkOpenTherm : public PollingComponent {
   void update() override;
 };
 
-// Logika dla suwaka/nastawy biegów
 inline void BrinkNumber::control(float value) {
   this->publish_state(value);
   if (this->parent_ != nullptr) {
@@ -79,46 +74,43 @@ inline void BrinkOpenTherm::update() {
   if (ot == nullptr) return;
   unsigned long response = 0;
 
-  // Podtrzymanie sesji
+  // Podtrzymanie
   ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)0, 0x0100));
-  if (this->status_sensor != nullptr) this->status_sensor->publish_state("OK");
 
   switch(current_step) {
-    case 0: // Sterowanie (Zapis do TSP 52 - Current Volume lub ID 71)
+    case 0: // Nastawa
       ot->sendRequest(ot->buildRequest(OpenThermMessageType::WRITE_DATA, (OpenThermMessageID)71, (unsigned int)target_ventilation));
       current_step++; break;
 
-    case 1: // T1 (TSP 57) - Offset -100
-      response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)89, 57 << 8));
-      if (ot->isValidResponse(response) && t_supply_in_sensor) t_supply_in_sensor->publish_state((float)(response & 0xFF) - 100);
+    case 1: // T1 - Czerpnia (Standard OT ID 80) - sprawdzamy standard zamiast TSP
+      response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)80, 0));
+      if (ot->isValidResponse(response) && t_supply_in_sensor) t_supply_in_sensor->publish_state(ot->getFloat(response));
       current_step++; break;
 
-    case 2: // T2 (TSP 69) - Offset -100
-      response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)89, 69 << 8));
-      if (ot->isValidResponse(response) && t_supply_out_sensor) t_supply_out_sensor->publish_state((float)(response & 0xFF) - 100);
+    case 2: // T3 - Wywiew (Standard OT ID 82)
+      response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)82, 0));
+      if (ot->isValidResponse(response) && t_exhaust_in_sensor) t_exhaust_in_sensor->publish_state(ot->getFloat(response));
       current_step++; break;
 
-    case 3: // T3 (TSP 58) - Offset -100
-      response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)89, 58 << 8));
-      if (ot->isValidResponse(response) && t_exhaust_in_sensor) t_exhaust_in_sensor->publish_state((float)(response & 0xFF) - 100);
+    case 3: // T2 - Nawiew (TSP 41 lub 42 w niektórych HR)
+      response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)89, 41 << 8));
+      if (ot->isValidResponse(response) && t_supply_out_sensor) t_supply_out_sensor->publish_state((float)(response & 0xFF));
       current_step++; break;
 
-    case 4: // T4 (TSP 70) - Offset -100
-      response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)89, 70 << 8));
-      if (ot->isValidResponse(response) && t_exhaust_out_sensor) t_exhaust_out_sensor->publish_state((float)(response & 0xFF) - 100);
+    case 4: // T4 - Wyrzutnia (TSP 43 lub 44)
+      response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)89, 43 << 8));
+      if (ot->isValidResponse(response) && t_exhaust_out_sensor) t_exhaust_out_sensor->publish_state((float)(response & 0xFF));
       current_step++; break;
 
-    case 5: // Odczyt aktualnego przepływu (TSP 62)
+    case 5: // Przepływ (TSP 62) - DZIAŁA
       response = ot->sendRequest(ot->buildRequest(OpenThermMessageType::READ_DATA, (OpenThermMessageID)89, 62 << 8));
       if (ot->isValidResponse(response) && current_flow_sensor) {
           float flow = (float)(response & 0xFF);
           current_flow_sensor->publish_state(flow);
-          
-          // Raportowanie biegu na podstawie przepływu (zgodnie z Twoją listą)
           if (current_gear_sensor) {
-            if (flow > 250) current_gear_sensor->publish_state("Bieg 3");
-            else if (flow > 150) current_gear_sensor->publish_state("Bieg 2");
-            else if (flow > 50) current_gear_sensor->publish_state("Bieg 1");
+            if (flow > 200) current_gear_sensor->publish_state("Bieg 3");
+            else if (flow > 120) current_gear_sensor->publish_state("Bieg 2");
+            else if (flow > 40) current_gear_sensor->publish_state("Bieg 1");
             else current_gear_sensor->publish_state("Bieg 0");
           }
       }
