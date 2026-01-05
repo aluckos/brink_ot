ee3d68e99a162d3854302c8d48bbea8578fdd07b
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-05 14:03:08 +0100

    Update binary_sensor.py

--- components/brink_ventilation/binary_sensor.py
+++ components/brink_ventilation/binary_sensor.py
@@ -2,17 +2,19 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
 from esphome.const import (
-    CONF_ID, CONF_TYPE, DEVICE_CLASS_PROBLEM, DEVICE_CLASS_CONNECTIVITY
+    CONF_ID, CONF_TYPE, DEVICE_CLASS_PROBLEM
 )
 from . import BRINK_VENTILATION_ID, BrinkOpenTherm
 
-# Definicje typów sensorów binarnych i ich domyślnych klas urządzeń
+# Definicje typów sensorów binarnych
+# Klucz musi odpowiadać metodzie set_..._binary w C++
 TYPES = {
     "FILTER_STATUS": DEVICE_CLASS_PROBLEM,
-    "CONNECTION_STATUS": DEVICE_CLASS_CONNECTIVITY,
 }
 
-CONFIG_SCHEMA = binary_sensor.binary_sensor_schema().extend({
+CONFIG_SCHEMA = binary_sensor.binary_sensor_schema(
+    device_class=DEVICE_CLASS_PROBLEM,
+).extend({
     cv.GenerateID(BRINK_VENTILATION_ID): cv.use_id(BrinkOpenTherm),
     cv.Required(CONF_TYPE): cv.one_of(*TYPES, upper=True),
 }).extend(cv.COMPONENT_SCHEMA)
@@ -23,12 +25,7 @@ async def to_code(config):
     # Tworzymy obiekt binary_sensor
     var = await binary_sensor.new_binary_sensor(config)
     
-    # Ustawiamy klasę urządzenia na podstawie typu, jeśli użytkownik nie podał własnej w YAML
-    if CONF_TYPE in config:
-        cg.add(var.set_device_class(TYPES[config[CONF_TYPE]]))
-
     # Łączymy z metodą w pliku brink_ot.h
-    # Typ "CONNECTION_STATUS" wywoła parent->set_connection_status_binary(var)
-    func_name = f"set_{config[CONF_TYPE].lower()}_binary"
-    func = getattr(parent, func_name)
+    # Przykład: type "FILTER_STATUS" -> wywoła parent.set_filter_status_binary(var)
+    func = getattr(parent, f"set_{config[CONF_TYPE].lower()}_binary")
     cg.add(func(var))
