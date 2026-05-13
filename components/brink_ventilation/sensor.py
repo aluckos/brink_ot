f4f8832c355e671bc5bab55a6ac0551723af16ea
aluckos <59487836+aluckos@users.noreply.github.com>
2026-05-13 07:17:40 +0200

    Update sensor.py

--- components/brink_ventilation/sensor.py
+++ components/brink_ventilation/sensor.py
@@ -2,13 +2,15 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import (
-    CONF_ID, CONF_TYPE, CONF_NAME, DEVICE_CLASS_TEMPERATURE, 
-    STATE_CLASS_MEASUREMENT, UNIT_CELSIUS
+    CONF_ID, 
+    CONF_TYPE, 
+    CONF_NAME, 
+    STATE_CLASS_MEASUREMENT, 
+    UNIT_CELSIUS
 )
 from . import BRINK_VENTILATION_ID, BrinkOpenTherm
 
 # Definicja dostępnych typów sensorów
-# Klucz: Nazwa typu w YAML
 TYPES = {
     "T_SUPPLY_IN": "Brink Temp Czerpnia (T1)",
     "T_SUPPLY_OUT": "Brink Temp Nawiew (T2)",
@@ -25,21 +27,28 @@ CONFIG_SCHEMA = sensor.sensor_schema(
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
+    # Pobieramy referencję do głównego komponentu (brink_ot.h)
     parent = await cg.get_variable(config[BRINK_VENTILATION_ID])
     
-    # Tworzymy obiekt sensora z parametrami z YAML
+    # Tworzymy obiekt sensora
     var = await sensor.new_sensor(config)
     
-    # Ustawiamy dokładność dla temperatur
     sensor_type = config[CONF_TYPE]
+    
+    # --- LOGIKA POPRAWIAJĄCA BŁĄD ---
+    
     if sensor_type in ["T_SUPPLY_IN", "T_SUPPLY_OUT", "T_EXHAUST_IN", "T_EXHAUST_OUT"]:
+        # Ustawienia dla temperatur
+        cg.add(var.set_unit_of_measurement(UNIT_CELSIUS))
         cg.add(var.set_accuracy_decimals(1))
-    else:
+    elif sensor_type == "CURRENT_FLOW":
+        # Ustawienia dla przepływu powietrza
+        cg.add(var.set_unit_of_measurement("m³/h"))
         cg.add(var.set_accuracy_decimals(0))
     
-    # Łączymy sensor z komponentem głównym (brink_ot.h)
-    # config[CONF_TYPE] zwraca np. "T_SUPPLY_OUT"
-    # .lower() zmienia to na "t_supply_out"
-    # f-string tworzy nazwę funkcji: "set_t_supply_out_sensor"
-    func = getattr(parent, f"set_{config[CONF_TYPE].lower()}_sensor")
+    # --- KONIEC POPRAWKI ---
+
+    # Łączymy sensor z odpowiednią funkcją "set_..._sensor" w brink_ot.h
+    func_name = f"set_{sensor_type.lower()}_sensor"
+    func = getattr(parent, func_name)
     cg.add(func(var))
