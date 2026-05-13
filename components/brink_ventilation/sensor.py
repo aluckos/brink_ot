ad883ccdffc1c9a33ecca9899ec0648a991e3237
aluckos <59487836+aluckos@users.noreply.github.com>
2026-05-13 07:41:53 +0200

    Update sensor.py

--- components/brink_ventilation/sensor.py
+++ components/brink_ventilation/sensor.py
@@ -14,9 +14,8 @@ TYPES = {
     "CURRENT_FLOW": "Brink Przepływ",
 }
 
-CONFIG_SCHEMA = sensor.sensor_schema(
-    state_class=STATE_CLASS_MEASUREMENT,
-).extend({
+# Używamy podstawowego schematu sensora, ale wymuszamy brak jednostki w definicji
+CONFIG_SCHEMA = sensor.sensor_schema().extend({
     cv.GenerateID(BRINK_VENTILATION_ID): cv.use_id(BrinkOpenTherm),
     cv.Required(CONF_TYPE): cv.one_of(*TYPES, upper=True),
 }).extend(cv.COMPONENT_SCHEMA)
@@ -24,33 +23,27 @@ CONFIG_SCHEMA = sensor.sensor_schema(
 async def to_code(config):
     parent = await cg.get_variable(config[BRINK_VENTILATION_ID])
     
-    # Tworzymy kopię, żeby nie psuć globalnej konfiguracji
-    new_config = config.copy()
-    
-    # Zapamiętujemy jednostkę i USUWAMY ją z configu dla generatora C++
-    # To zapobiegnie wygenerowaniu linii ->set_unit_of_measurement() w main.cpp
+    # Tworzymy sensor, ale ręcznie czyścimy jednostkę w obiekcie config
+    # zanim ESPHome zacznie generować kod C++
+    config_copy = config.copy()
     unit = ""
-    if new_config[CONF_TYPE] in ["T_SUPPLY_IN", "T_SUPPLY_OUT", "T_EXHAUST_IN", "T_EXHAUST_OUT"]:
+    if config_copy[CONF_TYPE] in ["T_SUPPLY_IN", "T_SUPPLY_OUT", "T_EXHAUST_IN", "T_EXHAUST_OUT"]:
         unit = UNIT_CELSIUS
-    elif new_config[CONF_TYPE] == "CURRENT_FLOW":
+    elif config_copy[CONF_TYPE] == "CURRENT_FLOW":
         unit = "m³/h"
+
+    # To jest najważniejsza linia: oszukujemy system, że nie ma jednostki
+    config_copy[CONF_UNIT_OF_MEASUREMENT] = ""
     
-    # Kluczowy moment: ustawiamy jednostkę na None w słowniku config
-    new_config[CONF_UNIT_OF_MEASUREMENT] = None
+    var = await sensor.new_sensor(config_copy)
     
-    # Generujemy sensor (teraz bez jednostki w C++)
-    var = await sensor.new_sensor(new_config)
-    
-    # Ręcznie przypisujemy jednostkę do obiektu w Pythonie (dla Home Assistant)
-    # ale bez generowania wywołania funkcji w C++
+    # Ręcznie wstrzykujemy jednostkę do metadanych (nie jako wywołanie funkcji C++)
     cg.add(var.set_unit_of_measurement(unit))
     
-    # Ustawiamy dokładność
-    if new_config[CONF_TYPE] == "CURRENT_FLOW":
+    if config_copy[CONF_TYPE] == "CURRENT_FLOW":
         cg.add(var.set_accuracy_decimals(0))
     else:
         cg.add(var.set_accuracy_decimals(1))
 
-    # Łączymy z głównym komponentem
     func = getattr(parent, f"set_{config[CONF_TYPE].lower()}_sensor")
     cg.add(func(var))
